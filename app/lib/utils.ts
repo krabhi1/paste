aad940bb66f6f843719025446ac57b676aec54f4
krabhi1 <abhishek33gl@gmail.com>
2025-09-10 21:44:39 +0530

    Remove IS_DEV and IS_TEST flags from utils and home route

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -8,6 +8,3 @@ export const pasteId = customAlphabet(
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-export const IS_DEV = process.env.NODE_ENV === "development";
-export const IS_TEST = process.env.NODE_ENV === "test";
